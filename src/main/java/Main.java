55c622ee7a445e11f1931e9a543649c516d232a6
ElizaHales <59296568+ElizaHales@users.noreply.github.com>
2022-02-08 19:37:31 -0700

    put default Calculator constructor all one one line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,31 +5,32 @@ public class Main {
     System.out.println("Welcome to the calculator!");
     System.out.println("Please enter a command:");
 
-    Calculator calculator = new Calculator();
-    Scanner inputObj = new Scanner(System.in);  // Create a Scanner object
-    String commandName = inputObj.next();
-    System.out.println("command name: " + commandName);
-    int firstArg = Integer.parseInt(inputObj.next());
-    System.out.println("firstArg: " + firstArg);
-    int secondArg = 0;
-    if (inputObj.hasNext()) {
-      secondArg = Integer.parseInt(inputObj.next());
-      System.out.println("secondArg: " + secondArg);
-    }
+    Scanner inputObj=new Scanner(System.in);
+    while (inputObj.hasNext()) {
+      Calculator calculator=new Calculator();
+      String commandName=inputObj.next();
+      System.out.println("command name: " + commandName);
+      int firstArg=Integer.parseInt(inputObj.next());
+      System.out.println("firstArg: " + firstArg);
+      int secondArg=0;
+      if (inputObj.hasNext()) {
+        secondArg=Integer.parseInt(inputObj.next());
+        System.out.println("secondArg: " + secondArg);
+      }
 
-    if (commandName.equals("add")) {
-      System.out.println(calculator.add(firstArg, secondArg));
-    } else if (commandName == "subtract") {
-      System.out.println(calculator.subtract(firstArg, secondArg));
-    } else if (commandName == "multiply") {
-      System.out.println(calculator.multiply(firstArg, secondArg));
-    } else if (commandName == "divide") {
-      System.out.println(calculator.divide(firstArg, secondArg));
-    } else if (commandName == "fibonacci") {
-      System.out.println(calculator.fibonacciNumberFinder(firstArg));
-    } else if (commandName == "binary") {
-      String binaryNumber = calculator.intToBinaryNumber(firstArg);
-      System.out.println(binaryNumber);
+      if (commandName.equals("add")) {
+        System.out.println(calculator.add(firstArg, secondArg));
+      } else if (commandName.equals("subtract")) {
+        System.out.println(calculator.subtract(firstArg, secondArg));
+      } else if (commandName.equals("multiply")) {
+        System.out.println(calculator.multiply(firstArg, secondArg));
+      } else if (commandName.equals("divide")) {
+        System.out.println(calculator.divide(firstArg, secondArg));
+      } else if (commandName.equals("fibonacci")) {
+        System.out.println(calculator.fibonacciNumberFinder(firstArg));
+      } else if (commandName.equals("binary")) {
+        System.out.println(calculator.intToBinaryNumber(firstArg));
+      }
     }
   }
 }
