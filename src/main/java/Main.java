7ae07baca0e88a27a4b4a65021154bc5584035d1
ElizaHales <59296568+ElizaHales@users.noreply.github.com>
2022-02-08 19:42:53 -0700

    fix while loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,27 +9,24 @@ public class Main {
     while (inputObj.hasNext()) {
       Calculator calculator=new Calculator();
       String commandName=inputObj.next();
-      System.out.println("command name: " + commandName);
       int firstArg=Integer.parseInt(inputObj.next());
-      System.out.println("firstArg: " + firstArg);
-      int secondArg=0;
       if (inputObj.hasNext()) {
-        secondArg=Integer.parseInt(inputObj.next());
-        System.out.println("secondArg: " + secondArg);
-      }
-
-      if (commandName.equals("add")) {
-        System.out.println(calculator.add(firstArg, secondArg));
-      } else if (commandName.equals("subtract")) {
-        System.out.println(calculator.subtract(firstArg, secondArg));
-      } else if (commandName.equals("multiply")) {
-        System.out.println(calculator.multiply(firstArg, secondArg));
-      } else if (commandName.equals("divide")) {
-        System.out.println(calculator.divide(firstArg, secondArg));
-      } else if (commandName.equals("fibonacci")) {
-        System.out.println(calculator.fibonacciNumberFinder(firstArg));
-      } else if (commandName.equals("binary")) {
-        System.out.println(calculator.intToBinaryNumber(firstArg));
+        int secondArg = Integer.parseInt(inputObj.next());
+        if (commandName.equals("add")) {
+          System.out.println(calculator.add(firstArg, secondArg));
+        } else if (commandName.equals("subtract")) {
+          System.out.println(calculator.subtract(firstArg, secondArg));
+        } else if (commandName.equals("multiply")) {
+          System.out.println(calculator.multiply(firstArg, secondArg));
+        } else if (commandName.equals("divide")) {
+          System.out.println(calculator.divide(firstArg, secondArg));
+        }
+      } else {
+        if (commandName.equals("fibonacci")) {
+          System.out.println(calculator.fibonacciNumberFinder(firstArg));
+        } else if (commandName.equals("binary")) {
+          System.out.println(calculator.intToBinaryNumber(firstArg));
+        }
       }
     }
   }
