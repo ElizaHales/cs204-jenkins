e05179fdc9251bb1060e23dabd28dac794ec3a65
ElizaHales <59296568+ElizaHales@users.noreply.github.com>
2022-02-08 19:13:02 -0700

    put default Calculator constructor all one one line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    System.out.println("Welcome to the calculator!");
+    System.out.println("Please enter a command:");
+
+    Calculator calculator = new Calculator();
+    Scanner inputObj = new Scanner(System.in);  // Create a Scanner object
+    String commandName = inputObj.next();
+    int firstArg = Integer.parseInt(inputObj.next());
+    int secondArg = 0;
+    if (inputObj.hasNext()) {
+      secondArg = Integer.parseInt(inputObj.next());
+    }
+
+    if (commandName == "add") {
+      System.out.println(calculator.add(firstArg, secondArg));
+    } else if (commandName == "subtract") {
+      System.out.println(calculator.subtract(firstArg, secondArg));
+    } else if (commandName == "multiply") {
+      System.out.println(calculator.multiply(firstArg, secondArg));
+    } else if (commandName == "divide") {
+      System.out.println(calculator.divide(firstArg, secondArg));
+    } else if (commandName == "fibonacci") {
+      System.out.println(calculator.fibonacciNumberFinder(firstArg));
+    } else if (commandName == "binary") {
+      System.out.println(calculator.intToBinaryNumber(firstArg));
+    }
   }
 }
